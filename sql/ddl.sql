2c5c6257180542bd69f787e11eded10ce3b2df62
henry <kyeel123@gmail.com>
2020-08-11 15:28:09 -0400

    more stuff done

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,20 +1,24 @@
 CREATE DATABASE EmployeeDB;
 CREATE TABLE Department(
-    int DepartmentID IDENTITY(1,1);
-    NVARCHAR(50) [Name];
-    NVARCHAR(50) [Location];
+    int DepartmentID IDENTITY(1,1) Primary Key,
+    NVARCHAR(50) [Name],
+    NVARCHAR(50) [Location],
 
 )
 
 CREATE TABLE Employee(
-    int EmployeeID Identity(1,1);
-    NVARCHAR(50) FirstName
-    NVARCHAR(50) LastName
-    int SSN;
-    int DeptID;
+    int EmployeeID Identity(1,1) PRIMARY Key,
+    NVARCHAR(50) FirstName,
+    NVARCHAR(50) LastName,
+    int SSN,
+    int DeptID,
+    CONSTRAINT fk_dept (DeptID) FOREIGN
 
 )
 
 
 
-Create table empDetails;
+Create table empDetails(
+    int empDetailsId Identity(1.1) PRIMARY KEY,
+
+)
